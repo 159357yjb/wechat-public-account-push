a55773b1b8756ba78f532ad0b2cb311dd34bb50b
159357yjb <113030406+159357yjb@users.noreply.github.com>
2022-09-08 00:04:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "ob6is59a3PalkgU_1S5rkEqQZhwo",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "dPHAHDdZA7fNanxIyPWJ813CG-2A065xN-5ysP4fHc0",
+   *    useTemplateId: "wzc1ESAqrA-GtX5hygJaQnfIobYzGpxq-Ou-gEAcIrs",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ob6is59a3PalkgU_1S5rkEqQZhwo",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dPHAHDdZA7fNanxIyPWJ813CG-2A065xN-5ysP4fHc0",
+      useTemplateId: "wzc1ESAqrA-GtX5hygJaQnfIobYzGpxq-Ou-gEAcIrs",
       // 所在省份
       province: "广东",
       // 所在城市
