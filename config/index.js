2c913cdee60e583c80b75933d4fcae87838b8027
159357yjb <113030406+159357yjb@users.noreply.github.com>
2022-09-08 20:40:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -83,7 +83,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "gh_0f4d2f23e91a",
     }, 
   ],
     
